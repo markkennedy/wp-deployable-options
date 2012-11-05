6b9100889ce6a32a822a402fc8af86e219e04072
Mark Kennedy <mark.kennedy@gmail.com>
2012-11-05 15:31:52 -0500

    First commit

---
+++ WP_Deployable_Options.php
@@ -0,0 +1,33 @@
+<?php
+class WP_Deployable_Options {
+	public function __construct() {
+	}
+
+	public function init() {
+		if(defined('WP_DEPLOYABLE_OPTIONS_JSON') && self::has_not_already_initialized()) {
+			$json = json_decode(WP_DEPLOYABLE_OPTIONS_JSON);
+			$this->add_filters_for_options($json);
+		}
+		self::has_initialized();
+	}
+
+	private function add_filters_for_options($json) {
+		foreach($json as $section) {
+			foreach ($section as $option_name => $value) {
+				$filter = 'pre_option_' . $option_name;
+				$callback = function() use($value) { return $value; };
+				add_filter($filter, $callback);
+			}
+		}
+	}
+
+	private static $has_initialized;
+
+	private static function has_not_already_initialized() {
+		return !self::$has_initialized;
+	}
+
+	private static function has_initialized() {
+		self::$has_initialized = true;
+	}
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Deployable Options
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Allows setting WordPress option values in a JSON file that can be deployed with other code.
+Version: 0.1
+Author: Mark Kennedy
+Author URI: http://URI_Of_The_Plugin_Author
+License: GPL2
+*/
+WP_Deployable_Options::init();
